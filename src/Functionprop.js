57b73082cecafeda47ee8032d51ff9cd6e528f1c
jshepdevelopment <jshepherd@outlook.com>
2020-09-12 10:58:51 -0600

    Update functional prop

--- src/Functionprop.js
+++ src/Functionprop.js
@@ -4,7 +4,7 @@ function Functionprops(props) {
     return (
         <div>
             <h3>This is working in a functional component.</h3>,
-            <h3>Welcome to functional component {props.name} from {props.place}.</h3>
+            <h3>Welcome to functional component that uses props passed as an argument {props.name} from {props.place}.</h3>
         </div>
     );
 }
